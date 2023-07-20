d0824ca02a0a4c01e9acaa189df6979cf76c45d5
Gab-off <albuquerque1.gabriel@gmail.com>
2023-07-20 09:57:20 -0300

    v2 do Bank - add Scanner para valores dinamicos

--- src/co/gabrielmiranda/bank/App.java
+++ src/co/gabrielmiranda/bank/App.java
@@ -1,27 +1,33 @@
 package co.gabrielmiranda.bank;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
         Account account = new Account("0001", "1234", "Seu Barriga");
 
-        boolean succeed = account.withDraw(200.0);
-        if (!succeed) {
-            System.out.println("Voce nao tem saldo suficiente para sacar");
+        while (true) {
+            System.out.println("O que deseja fazer? D=Deposito, S=Saque, E=Sair da conta");
+            String op = scanner.nextLine().toUpperCase();
+            if (op.equals("D")) {
+                System.out.println("Qual valor deseja depositar?");
+                double value = scanner.nextDouble();
+                account.deposit(value);
+            } else if (op.equals("S")) {
+                System.out.println("Qual valor deseja sacar?");
+                double value = scanner.nextDouble();
+                if (!account.withDraw(value)) {
+                    System.out.println("Ops! Não foi possível sacar o valor R$" + value);
+                }
+            } else if (op.equals("E")) {
+                break;
+            } else {
+                System.out.println("Comando inválido, tente novamente!");
+            }
+            scanner = new Scanner(System.in);
         }
-
-        account.deposit(100);
-        account.deposit(50);
-        account.deposit(100);
-
-        if (!account.withDraw(200)) {
-            System.out.println("Voce nao tem saldo suficiente para sacar");
-        }
-
-        if (!account.withDraw(200)) {
-            System.out.println("Voce nao tem saldo suficiente para sacar");
-        }
-
-        System.out.println(account);
+        scanner.close();
     }
 }
