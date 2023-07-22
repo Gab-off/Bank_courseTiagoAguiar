8d4afb0411938cf75d01ba246051c492a8e967d9
Gab-off <albuquerque1.gabriel@gmail.com>
2023-07-22 19:31:19 -0300

    metade do vídeo da V3

--- src/co/gabrielmiranda/bank/App.java
+++ src/co/gabrielmiranda/bank/App.java
@@ -5,6 +5,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Bank santander = new Bank("0001");
+
+        while(true) {
+            System.out.println("O que deseja fazer? C=Criar conta; E=Sair do programa");
+            String op = scanner.nextLine();
+
+            if (op.equals("C")) {
+                System.out.println("Digite o seu nome:");
+                String name = scanner.nextLine();
+                Account account = santander.generateAccount(name);
+                System.out.println(account);
+            } else if (op.equals("E")) {
+                break;
+            } else {
+                System.out.println("Comando inválido, tente novamente");
+            }
+        }
+
 
         Account account = new Account("0001", "1234", "Seu Barriga");
 
